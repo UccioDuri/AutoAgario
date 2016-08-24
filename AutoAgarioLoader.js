83ba5a4ac2aeee7dc9ec2ee01c061860e194e218
UccioDuri <uccio_7@tiscali.it>
2016-08-24 23:46:13 +0200

    Update AutoAgarioLoader.js

--- AutoAgarioLoader.js
+++ AutoAgarioLoader.js
@@ -14,69 +14,30 @@
 console.log('Inizia AutoAgario2');
 
     var urlScriptDaModificare = 'http://jeu.video/agario/js/modes/iframe/agario_en.js';
+    var urlScriptSupporto = 'https://raw.githubusercontent.com/UccioDuri/AutoAgario/master/AutoAgarioScript.js'
     var Contenitore;
-    var ScriptDaModificare;
-    var ScriptNuovo;
-    var BackupTestoScript;
-    var TestoScript;
-    var xhttp;
-    var FintoMouseX;
-    var FintoMouseY;
-
+    var ScriptSupporto;
+    
     function TrovaScript() {
         var ScriptCaricati = document.getElementsByTagName('script');
         for (var i = ScriptCaricati.length; i >= 0; i--) {
             if (ScriptCaricati[i] && ScriptCaricati[i].getAttribute('src') !== null && ScriptCaricati[i].getAttribute('src').indexOf(urlScriptDaModificare) != -1 ) {
-                ScriptDaModificare = ScriptCaricati[i];
-                Contenitore = ScriptDaModificare.parentNode;
-console.log('Trovato: ' + ScriptDaModificare.getAttribute('src'));
+                Contenitore = ScriptCaricati[i].parentNode;
+console.log('Trovato: ' + ScriptCaricati[i].getAttribute('src'));
                 break;
             }
         }
     }
 
-    function RichiediScript() {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = ModificaScript;
-        xhttp.open("GET", ScriptDaModificare.getAttribute('src'), true);
-        xhttp.send();
-console.log('Richiesto: ' + ScriptDaModificare.getAttribute('src'));
-    }
-
-    function ModificaScript() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            BackupTestoScript = xhttp.responseText;
-console.log('Scaricato');
-
-            var EspRegX = /[a-zA-Z0-9.]+\.clientX/g;
-            var EspRegY = /[a-zA-Z0-9.]+\.clientY/g;
-            TestoScript = BackupTestoScript.replace(EspRegX, 'FintoMouseX');
-            TestoScript = TestoScript.replace(EspRegY, 'FintoMouseY');
-console.log('Modificato');
-        }
-    }
-
-    function CambiaScript() {
-        ScriptNuovo = document.createElement("script");
-        ScriptNuovo.type = 'text/javascript';
-        ScriptNuovo.innerHTML = TestoScript;
-        Contenitore.removeChild(ScriptDaModificare);
-        Contenitore.appendChild(ScriptNuovo);
-console.log('Cambiato');
-    }
-
-    function RipristinaScript() {
-        ScriptDaModificare = document.createElement("script");
-        ScriptDaModificare.type = 'text/javascript';
-        ScriptDaModificare.innerHTML = BackupTestoScript;
-        Contenitore.removeChild(ScriptNuovo);
-        Contenitore.appendChild(ScriptDaModificare);
-console.log('Ripristinato');
+    function CreaScriptSupporto() {
+        ScriptSupporto = document.createElement("script");
+        ScriptSupporto.type = 'text/javascript';
+        ScriptSupporto.setAttribute('src', urlScriptSupporto);
+        Contenitore.appendChild(ScriptSupporto);
+console.log('Creato');
     }
 
     TrovaScript();
-    RichiediScript();
-    CambiaScript();
-    RipristinaScript();
-
+    CreaScriptSupporto();
+    
 })();
