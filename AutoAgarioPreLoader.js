364a6c75bdd79aae1dda382b6670a5e5ea5eafc7
UccioDuri <uccio_7@tiscali.it>
2016-08-25 15:12:55 +0200

    Update AutoAgarioPreLoader.js

--- AutoAgarioPreLoader.js
+++ AutoAgarioPreLoader.js
@@ -1,21 +1,42 @@
-document.addEventListener("beforescriptexecute", function(e) {
+// ==UserScript==
+// @name         AutoAgarioPreLoader
+// @namespace    AutoAgarioByUccio
+// @version      0.0.0.1
+// @description  Bot for Allkeyshop.com Agario
+// @author       Uccio
+// @match        http://jeu.video/agario/iframe/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/UccioDuri/AutoAgario/master/AutoAgarioPreLoader.js
+// @homepage     https://www.twitch.tv/uccioduri
+// ==/UserScript==
 
-    src = e.target.src;
-    content = e.target.text;
+//debugger;
+(function() {
+    'use strict';
 
-    if (src.search(urlScriptDaModificare) > -1) {
-        // Stop original script
-        e.preventDefault();
-        e.stopPropagation();
-        window.jQuery(e.target).remove();
+console.log('Inizia AutoAgarioPreLoader');
 
-//        var script = document.createElement('script');
+    document.addEventListener("beforescriptexecute", function(e) {
 
-//        script.textContent = 'script you want';
+        src = e.target.src;
+        content = e.target.text;
 
-//        (document.head || document.documentElement).appendChild(script);
-//        script.onload = function() {
-//            this.parentNode.removeChild(this);
-//        }
-    }
-}, false);
+        if (src.search(urlScriptDaModificare) > -1) {
+console.log('Trovato script');
+            // Stop original script
+            e.preventDefault();
+            e.stopPropagation();
+            window.jQuery(e.target).remove();
+
+//            var script = document.createElement('script');
+
+//            script.textContent = 'script you want';
+
+//            (document.head || document.documentElement).appendChild(script);
+//            script.onload = function() {
+//                this.parentNode.removeChild(this);
+//            }
+        }
+    }, false);
+
+})();
