813650f9342fa108fc8b9b53aabb4f498de44759
UccioDuri <uccio_7@tiscali.it>
2016-08-25 14:53:37 +0200

    Update AutoAgarioScript.js

--- AutoAgarioScript.js
+++ AutoAgarioScript.js
@@ -14,6 +14,28 @@ var StatoAutoAgario = false;
 var FintoMouseX;
 var FintoMouseY;
 
+document.addEventListener("beforescriptexecute", function(e) {
+
+    src = e.target.src;
+    content = e.target.text;
+
+    if (src.search(urlScriptDaModificare) > -1) {
+        // Stop original script
+        e.preventDefault();
+        e.stopPropagation();
+        window.jQuery(e.target).remove();
+
+//        var script = document.createElement('script');
+
+//        script.textContent = 'script you want';
+
+//        (document.head || document.documentElement).appendChild(script);
+//        script.onload = function() {
+//            this.parentNode.removeChild(this);
+//        }
+    }
+}
+
 function TrovaScript() {
     var ScriptCaricati = document.getElementsByTagName('script');
     for (var i = ScriptCaricati.length; i >= 0; i--) {
