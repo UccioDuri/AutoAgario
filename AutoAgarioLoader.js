0c02adcbbf386a0a711593cbd21c5d8192aac11c
UccioDuri <uccio_7@tiscali.it>
2016-08-25 02:42:13 +0200

    Update AutoAgarioLoader.js

--- AutoAgarioLoader.js
+++ AutoAgarioLoader.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoAgario
 // @namespace    http://tampermonkey.net/
-// @version      0.2.0.8
+// @version      0.2.0.9
 // @description  Bot for Agario
 // @author       Uccio
 // @match        http://jeu.video/agario/iframe/*
